60b59f924345bc41d67b1920f94585ca54978710
jruby -S gem install bundler <newbluelight1031@gmail.com>
2019-08-23 19:10:35 -0700

    setting up enviromental variable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,9 +260,13 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
 #  config.omniauth :google_oauth2, 'APP_ID', 'APP_SECRET'
+#  config.omniauth :google_oauth2,
+        #          Rails.application.credentials.google_client_id,
+        #          Rails.application.credentials.google_client_secret
+
   config.omniauth :google_oauth2,
-                  Rails.application.credentials.google_client_id,
-                  Rails.application.credentials.google_client_secret
+      ENV['GOOGLE_APP_ID'],
+      ENV['GOOGLE_APP_SECRET'],
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
