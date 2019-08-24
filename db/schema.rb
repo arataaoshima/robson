38907c285cabe7d45e2e015a3a8c222115756b8d
jruby -S gem install bundler <newbluelight1031@gmail.com>
2019-08-23 18:38:26 -0700

    introduce google login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_010013) do
+ActiveRecord::Schema.define(version: 2019_08_24_011649) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2019_08_24_010013) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "token"
+    t.time "current_sign_in_at"
+    t.time "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.integer "sign_in_count"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
